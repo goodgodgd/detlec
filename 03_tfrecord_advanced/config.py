974814872c413c8776eb00ff48e599c4d6cd571f
goodgodgd <goodgodgd@yonsei.ac.kr>
2021-03-24 22:48:09 +0900

    structuring tfrecord maker ...

---
+++ 03_tfrecord_advanced/config.py
@@ -0,0 +1,35 @@
+import os.path as op
+
+
+class Config:
+    class Paths:
+        RAW_DATA = {"kitti": ""}
+        RESULT_ROOT = "/home/ian/workspace/detlec/dataset"
+        TFRECORD = op.join(RESULT_ROOT, "tfrecord")
+
+    class Dataset:
+        DATASETS_FOR_TFRECORD = {"kitti": ("train", "val", "test")}
+        INPUT_RESOLUTIONS = {"kitti": (300, 1000)}
+
+    class Train:
+        BATCH_SIZE = 2
+
+    @classmethod
+    def get_img_shape(cls, code="HW", dataset="kitti", scale_div=1):
+        imsize = cls.Dataset.INPUT_RESOLUTIONS[dataset]
+        if code == "H":
+            return imsize[0] // scale_div
+        elif code == "W":
+            return imsize[1] // scale_div
+        elif code == "HW":
+            return imsize[0] // scale_div, imsize[1] // scale_div
+        elif code == "WH":
+            return imsize[1] // scale_div, imsize[0] // scale_div
+        elif code == "HWC":
+            return imsize[0] // scale_div, imsize[1] // scale_div, 3
+        elif code == "BHWC":
+            return cls.Train.BATCH_SIZE, imsize[0] // scale_div, imsize[1] // scale_div, 3
+        else:
+            assert 0, f"Invalid code: {code}"
+
+
---
+++ 03_tfrecord_advanced/create_tfrecord_main.py
@@ -0,0 +1,27 @@
+import os.path as op
+import numpy as np
+import tensorflow as tf
+tf.config.set_visible_devices([], 'GPU')
+
+from config import Config as cfg
+import tfrecord_writer as tw
+
+
+def create_tfrecords():
+    datasets = cfg.Dataset.DATASETS_FOR_TFRECORD
+    for dataset, splits in datasets.items():
+        for split in splits:
+            tfrpath = op.join(cfg.Paths.TFRECORD, f"{dataset}_{split}")
+            if op.isdir(tfrpath):
+                print("[convert_to_tfrecords] tfrecord already created in", op.basename(tfrpath))
+                continue
+
+            srcpath = cfg.Paths.RAW_DATA[dataset]
+            dstshape = cfg.get_img_shape("HW", dataset)
+            tfrmaker = tw.tfrecord_maker_factory(dataset, split, srcpath, tfrpath, dstshape)
+            tfrmaker.make()
+
+
+if __name__ == "__main__":
+    np.set_printoptions(precision=3, suppress=True)
+    create_tfrecords()
