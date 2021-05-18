finally fix the bug, divide_no_nan() results in nan gradient

04_yolov3/model/head.py
@@ -129,7 +129,7 @@ class FeatureDecoder:
         """
         num_anc, channel = anchors_ratio.shape     # (3, 2)
         anchors_tf = tf.reshape(anchors_ratio, (1, 1, 1, num_anc, channel))
-        # Note: exp activation may result in infinity
+        # NOTE: exp activation may result in infinity
         # hw_dec = tf.exp(hw_raw) * anchors_tf
         # hw_dec: 0~3 times of anchor, the delayed sigmoid passes through (0, 1)
         hw_dec = self.const_3 * tf.sigmoid(hw_raw - self.const_log_2) * anchors_tf

04_yolov3/train/loss_pool.py
@@ -44,10 +44,14 @@ class CiouLoss(LossBase):
         c = tf.reduce_sum(cbox_hw * cbox_hw, axis=-1)
         center_diff = grtr_yxhw[..., :2] - pred_yxhw[..., :2]
         u = tf.reduce_sum(center_diff * center_diff, axis=-1)
-        d = tf.math.divide_no_nan(u, c)
+        # NOTE: divide_no_nan results in nan gradient
+        # d = tf.math.divide_no_nan(u, c)
+        d = u / (c + 1.0e-5)
 
-        grtr_hw_ratio = tf.math.divide_no_nan(grtr_yxhw[..., 2], grtr_yxhw[..., 3])
-        pred_hw_ratio = tf.math.divide_no_nan(pred_yxhw[..., 2], pred_yxhw[..., 3])
+        # grtr_hw_ratio = tf.math.divide_no_nan(grtr_yxhw[..., 2], grtr_yxhw[..., 3])
+        # pred_hw_ratio = tf.math.divide_no_nan(pred_yxhw[..., 2], pred_yxhw[..., 3])
+        grtr_hw_ratio = grtr_yxhw[..., 2] / (grtr_yxhw[..., 3] + 1.0e-5)
+        pred_hw_ratio = pred_yxhw[..., 2] / (pred_yxhw[..., 3] + 1.0e-5)
         coeff = tf.convert_to_tensor(4.0 / (np.pi * np.pi), dtype=tf.float32)
         v = coeff * tf.pow((tf.atan(grtr_hw_ratio) - tf.atan(pred_hw_ratio)), 2)
         alpha = v / (1 - iou + v)
@@ -56,7 +60,7 @@ class CiouLoss(LossBase):
         return loss
 
 
-class ObjectnessLoss:
+class ObjectnessLoss(LossBase):
     def __call__(self, grtr, pred, auxi):
         """
         :param grtr: dict of merged GT feature map slices, {key: (batch, HWA, dim), ...}
@@ -78,7 +82,7 @@ class ObjectnessLoss:
         return scalar_loss, obj_loss
 
 
-class CategoryLoss:
+class CategoryLoss(LossBase):
     def __call__(self, grtr, pred, auxi):
         """
         :param grtr: dict of merged GT feature map slices, {key: (batch, HWA, dim), ...}

04_yolov3/train/train_val.py
@@ -31,16 +31,14 @@ class TrainValBase:
         model_log = ModelLog()
         for step, features in enumerate(dataset):
             start = timer()
-            prediction, total_loss, loss_by_type = self.run_batch(features)
-            model_log.append_batch_result(step, features, prediction, total_loss, loss_by_type)
+            prediction, total_loss, loss_by_type, grad, weight = self.run_batch(features)
+            model_log.append_batch_result(step, features, prediction, total_loss, loss_by_type, grad, weight)
             uf.print_progress(f"training {step}/{self.epoch_steps} steps, "
                               f"time={timer() - start:.3f}, "
-                              f"feat_dl_max={np.around(np.max(np.abs(prediction['feature_l'].numpy())), 5)}, "
-                              f"feat_rl_max={np.around(np.max(np.abs(prediction['feature_l_raw'].numpy())), 5)}, "
-                              f"back_rl_max={np.around(np.max(np.abs(prediction['backbone_l_raw'].numpy())), 5)}, "
+                              f"feat_dl_max={np.max(np.abs(prediction['feature_l'].numpy())):.5f}, "
+                              f"feat_rl_max={np.max(np.abs(prediction['feature_l_raw'].numpy())):.5f}, "
+                              f"back_rl_max={np.max(np.abs(prediction['backbone_l_raw'].numpy())):.5f}, "
                               )
-            if step % 200 == 10:
-                print("")
             # if step > 20:
             #     break
 
@@ -64,32 +62,10 @@ class ModelEagerTrainer(TrainValBase):
             prediction = self.model(features["image"])
             total_loss, loss_by_type = self.loss_object(features, prediction)
 
-        self.check_nan(loss_by_type, features, prediction)
         grads = tape.gradient(total_loss, self.model.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
-        return prediction, total_loss, loss_by_type
-
-    def check_nan(self, losses, grtr, pred):
-        for name, loss in losses.items():
-            loss = loss.numpy()
-            if (loss.size > 1) and np.isnan(loss).any():
-                print(f"[train] nan loss:", name, np.quantile(loss, np.linspace(0, 1, 11)))
-            if loss.size == 1 and np.isnan(loss):
-                print(f"[train] nan loss:", name, loss)
-            if (loss.size > 1) and np.isinf(loss).any():
-                print(f"[train] inf loss:", name, np.quantile(loss, np.linspace(0, 1, 11)))
-            if loss.size == 1 and np.isinf(loss).any():
-                print(f"[train] inf loss:", name, loss)
-        for name, tensor in pred.items():
-            if np.isnan(tensor).any():
-                print(f"[train] nan pred:", name, np.quantile(tensor.numpy(), np.linspace(0, 1, 11)))
-            if np.isinf(tensor).any():
-                print(f"[train] inf pred:", name, np.quantile(tensor.numpy(), np.linspace(0, 1, 11)))
-        for name, tensor in grtr.items():
-            if np.isnan(tensor).any():
-                print(f"[train] nan grtr:", name, np.quantile(tensor.numpy(), np.linspace(0, 1, 11)))
-            if np.isinf(tensor).any():
-                print(f"[train] inf grtr:", name, np.quantile(tensor.numpy(), np.linspace(0, 1, 11)))
+        valid_grads = [tf.where(tf.math.is_nan(grad), 0., grad) for grad in grads]
+        self.optimizer.apply_gradients(zip(valid_grads, self.model.trainable_weights))
+        return prediction, total_loss, loss_by_type, grads, self.model.trainable_weights
 
 
 class ModelGraphTrainer(ModelEagerTrainer):
@@ -111,7 +87,7 @@ class ModelEagerValidater(TrainValBase):
     def validate_step(self, features):
         prediction = self.model(features["image"])
         total_loss, loss_by_type = self.loss_object(features, prediction)
-        return prediction, total_loss, loss_by_type
+        return prediction, total_loss, loss_by_type, (tf.constant(0.),), (tf.constant(0.),)
 
 
 class ModelGraphValidater(ModelEagerValidater):